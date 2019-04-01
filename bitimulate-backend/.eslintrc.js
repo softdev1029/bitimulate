351875e4619bb6a3376361d6a8049e964046c2ae
softdev1029 <softdev1029@outlook.com>
2019-04-02 03:51:43 +0800

    EP2 - project create

---
+++ bitimulate-backend/.eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "indent": [
+            "warn",
+            2
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "no-unused-vars": 1,
+        "eol-last": 0
+    }
+};
