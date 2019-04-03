636e1709e4a938b814a7569957573618e77bd47e
softdev1029 <softdev1029@outlook.com>
2019-04-04 01:08:31 +0800

    EP8 jwt middleware (when login api, re-generate token after 3 days)

--- bitimulate-backend/src/index.js
+++ bitimulate-backend/src/index.js
@@ -8,6 +8,7 @@ const bodyParser = require("koa-body-parser");
 const Router = require("koa-router");
 const api = require("./api");
 const db = require("db");
+const jwtMiddleware = require("lib/middlewares/jwt");
 const app = new Koa();
 
 // db
@@ -18,6 +19,7 @@ const router = new Router();
 router.use("/api", api.routes());
 
 app.use(bodyParser());
+app.use(jwtMiddleware);
 app.use(router.routes());
 app.use(router.allowedMethods());
 
