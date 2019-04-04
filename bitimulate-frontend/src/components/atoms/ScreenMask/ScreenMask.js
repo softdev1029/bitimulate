6846d1efa0f9d06f0bb566c8ff5a72d1d65e13ff
softdev1029 <softdev1029@outlook.com>
2019-04-05 04:09:13 +0800

    apply transitionGroup to ScreenMask

--- bitimulate-frontend/src/components/atoms/ScreenMask/ScreenMask.js
+++ bitimulate-frontend/src/components/atoms/ScreenMask/ScreenMask.js
@@ -1,11 +1,21 @@
 import React from "react";
 import styles from "./ScreenMask.scss";
 import classNames from "classnames/bind";
+import CSSTransitionGroup from "react-transition-group/CSSTransitionGroup";
 
 const cx = classNames.bind(styles);
 
 const ScreenMask = ({ visible }) => (
-  <div className={cx({ "screen-mask": visible })} />
+  <CSSTransitionGroup
+    transitionEnterTimeout={150}
+    transitionLeaveTimeout={150}
+    transitionName={{
+      enter: cx("fade-enter"),
+      leave: cx("fade-leave")
+    }}
+  >
+    {visible && <div className={cx("screen-mask")} />}
+  </CSSTransitionGroup>
 );
 
 ScreenMask.defaultProps = {};
