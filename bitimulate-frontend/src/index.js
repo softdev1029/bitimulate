b6f2d0f884326f772599f110538d53211d93c53f
softdev1029 <softdev1029@outlook.com>
2019-04-05 04:41:49 +0800

    react-hot-loader

--- bitimulate-frontend/src/index.js
+++ bitimulate-frontend/src/index.js
@@ -1,11 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { AppContainer } from "react-hot-loader";
 import "styles/main.scss";
 import Root from "Root";
 import * as serviceWorker from "./serviceWorker";
-import mystore from "store";
+import store from "store";
 
-ReactDOM.render(<Root store={mystore} />, document.getElementById("root"));
+const render = Component =>
+  ReactDOM.render(
+    <AppContainer>
+      <Component store={store} />
+    </AppContainer>,
+
+    document.getElementById("root")
+  );
+
+render(Root);
+
+if (module.hot) {
+  module.hot.accept("./Root", () => render(Root));
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
